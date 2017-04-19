33d2cfbd520670a2c92864fc1bd8f5ed25cc18c5
Ben Dworkin <ben.s.dworkin@gmail.com>
2017-04-18 19:43:15 -0500

    This

---
+++ app/src/main/java/com/bendworkin/safestroll/PickAlarm.java
@@ -0,0 +1,13 @@
+package com.bendworkin.safestroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_alarm);
+    }
+}
